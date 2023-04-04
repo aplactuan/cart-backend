414596d64e49534f23022b63ec75f633cf848920
aplactuan <aplactuan@gmail.com>
2023-04-04 20:30:44 +0800

    Check if the cart is empty

--- app/Http/Controllers/Cart/UserItemsController.php
+++ app/Http/Controllers/Cart/UserItemsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Cart;
 
+use App\Cart\Cart;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Cart\CartResource;
 use Illuminate\Http\Request;
@@ -14,10 +15,21 @@ class UserItemsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return CartResource
      */
-    public function __invoke(Request $request): CartResource
+    public function __invoke(Request $request, Cart $cart): CartResource
     {
         $request->user()->load(['cart.product', 'cart.product.variations.stock', 'cart.stock']);
 
-        return new CartResource($request->user());
+        return (new CartResource(
+            $request->user()
+        ))->additional([
+            'meta' => $this->meta($cart)
+        ]);
+    }
+
+    protected function meta($cart)
+    {
+        return [
+            'empty' => $cart->isEmpty()
+        ];
     }
 }
