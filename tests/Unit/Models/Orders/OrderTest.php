81a9b4c700dd3eb8b40634f83263e4fed6e1c25e
aplactuan <aplactuan@gmail.com>
2023-06-04 23:37:40 +0800

    Unit test for order model

---
+++ tests/Unit/Models/Orders/OrderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Models\Orders;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_belongs_to_a_user()
+    {
+        
+    }
+
+    public function test_it_belongs_to_an_address()
+    {
+        
+    }
+
+    public function test_it_belongs_to_a_shipping_method()
+    {
+        
+    }
+}
